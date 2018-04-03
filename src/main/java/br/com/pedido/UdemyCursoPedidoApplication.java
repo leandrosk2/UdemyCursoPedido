3cf77bd9f037ad61e9502eadd1d84f67dcd2b95b
leandrosk2 <leandrolima2985@gmail.com>
2018-04-02 22:14:47 -0300

    Criando Projeto

---
+++ src/main/java/br/com/pedido/UdemyCursoPedidoApplication.java
@@ -0,0 +1,12 @@
+package br.com.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCursoPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyCursoPedidoApplication.class, args);
+	}
+}
